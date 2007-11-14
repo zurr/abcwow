eb1c56e94e09da532ba95cd44a0eba471e9cb406
adamec.michal <adamec.michal@23d58eba-e73e-0410-97c2-2b01e85c642f>
2007-11-14 23:47:25 +0000

    - Added clean ascent 2319 without revisions 2318 (conflicts), 2312 and 2313 (partial 2.3 support)

---
+++ trunk/trunk_2319/src/shared/svn_revision.h
@@ -0,0 +1,16 @@
+// $Id: svn_revision.h 2303 2007-11-12 17:15:33Z burlex $
+
+#ifndef _SVN_REVISION_H
+#define _SVN_REVISION_H
+
+static const char * REVISION_TEXT	= "$Rev: 2303 $";
+static const char * BUILD_TAG		= "2.2-TRUNK";
+
+inline int g_getRevision()
+{
+	const char * p = REVISION_TEXT + 6;
+	return atoi(p);
+}
+
+/////////////////////////////////////////////////////////
+#endif 
