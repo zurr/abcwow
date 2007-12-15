2942e021031eddaf96c8cf2e500429030e254ae5
alex_portnov <alex_portnov@yahoo.com>
2007-12-15 12:56:30 +0000

    + Shadowstep won't port the user into the target model on scaled models. + 0.1f added to shadowstep Z coordinate to partially avoid porting underground while target on slop (upto ~30 deg.) .

--- trunk_2319/src/game/SpellEffects.cpp
+++ trunk_2319/src/game/SpellEffects.cpp
@@ -1093,6 +1093,7 @@ void Spell::SpellEffectTeleportUnits(uint32 i)  // Teleport Units
 	{
 		/* this is rather tricky actually. we have to calculate the orientation of the creature/player, and then calculate a little bit of distance behind that. */
 		float ang;
+
 		if(unitTarget == m_caster)
 		{
 			/* try to get a selection */
@@ -1122,13 +1123,13 @@ void Spell::SpellEffectTeleportUnits(uint32 i)  // Teleport Units
 			/* Players orientation is sent in movement packets */
 			ang = unitTarget->GetOrientation();
 		}
-
-		const static float shadowstep_distance = 1.0f;			/* 1.0 feet behind the target */
+		// avoid teleporting into the model on scaled models
+		const static float shadowstep_distance = 1.6f * unitTarget->GetFloatValue(OBJECT_FIELD_SCALE_X);
 		float new_x = unitTarget->GetPositionX() - (shadowstep_distance * cosf(ang));
 		float new_y = unitTarget->GetPositionY() - (shadowstep_distance * sinf(ang));
 		
 		/* Send a movement packet to "charge" at this target. Similar to warrior charge. */
-		p_caster->SafeTeleport(p_caster->GetMapId(), p_caster->GetInstanceID(), LocationVector(new_x, new_y, unitTarget->GetPositionZ(), unitTarget->GetOrientation()));
+		p_caster->SafeTeleport(p_caster->GetMapId(), p_caster->GetInstanceID(), LocationVector(new_x, new_y, (unitTarget->GetPositionZ() + 0.1f), unitTarget->GetOrientation()));
 		
 		return;
 	}
