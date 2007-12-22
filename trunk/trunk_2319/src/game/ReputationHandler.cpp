402bb2425de8ac7d946b4baf862eb16e3f5bd018
alex_portnov@yahoo.com <alex_portnov@yahoo.com@23d58eba-e73e-0410-97c2-2b01e85c642f>
2007-12-22 15:14:46 +0000

    + Quest giver and quest for "deal with the devil" item added.  - feel free adjusting the required/dialogs. + Crash in faction change aura.
    -- TODO: Properly test.
    -- Think what we about to do with those :
    SELECT * FROM items where allowablerace != -1
     - I think all weapons/armors should become usable by every race. There is no regular way as it is getting them besides .additem for playesrs from opposite factions.

--- trunk/trunk_2319/src/game/ReputationHandler.cpp
+++ trunk/trunk_2319/src/game/ReputationHandler.cpp
@@ -144,11 +144,6 @@ void Player::_InitialReputation()
 	uint32 j;
 	uint32 RaceMask = getRaceMask();
 
-	for(ReputationMap::iterator itr = m_reputation.begin(); itr != m_reputation.end(); ++itr)
-		delete itr->second;
-	for(uint32 i = 0; i < 128; ++i)
-		reputationByListId[i] = NULL;
-
 	for(uint32 i = 0; i < dbcFaction.GetNumRows(); ++i)
 	{
 		f = dbcFaction.LookupRow(i);
