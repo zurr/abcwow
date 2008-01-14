20a03345c8b192fb7452e941944d78103ba93cf9
teh.H4ck0r <teh.H4ck0r@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-01-14 15:21:25 +0000

    Life Tap cant kill someone now (wtf didnt i put that on last time already?) Hellfire Ramparts correction Magtheridon small cosmetic change :)

--- trunk_3052/src/scripts/src/InstanceScripts/Raid_Magtheridons_Lair.cpp
+++ trunk_3052/src/scripts/src/InstanceScripts/Raid_Magtheridons_Lair.cpp
@@ -218,8 +218,8 @@ public:
 					_unit->PlaySoundToSet(10253);
 					_unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "I... am... unleashed!");
 					m_phase = 2;
-					_unit->RemoveAuraVisual(BANISH2, 1);
 					_unit->SetUInt64Value(UNIT_FIELD_FLAGS, 0);
+					_unit->RemoveAuraVisual(BANISH2, 1);
 					_unit->GetAIInterface()->SetAllowedToEnterCombat(true);
 					_unit->GetAIInterface()->AttackReaction(target, 0, 0);
 				}
