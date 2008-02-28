27cb19f4255b3b3e36e972f3322c84c3d195c5f3
adamec.michal <adamec.michal@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-02-28 00:30:58 +0000

    - *broken* quest scripts

--- trunk/trunk_3944/src/scripts/src/QuestScripts/Setup.h
+++ trunk/trunk_3944/src/scripts/src/QuestScripts/Setup.h
@@ -1,4 +1,8 @@
 #ifndef QUEST_SCRIPTS_SETUP_H
 #define QUEST_SCRIPTS_SETUP_H
 
+void SetupFelOrcScavengers(ScriptMgr * mgr);
+void SetupGordunniOgres(ScriptMgr * mgr);
+void SetupKilsorrowAgents(ScriptMgr * mgr);
+
 #endif
