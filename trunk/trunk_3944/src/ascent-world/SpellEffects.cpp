2bd92279ed8b3289bff5f0c31bc6918d273c0f14
alex_portnov@yahoo.com <alex_portnov@yahoo.com@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-03-08 02:24:56 +0000

    + Fixed my typo in pickpocketing check code - tested this time. + Bonuses from quivers/pouches fixed.

--- trunk/trunk_3944/src/ascent-world/SpellEffects.cpp
+++ trunk/trunk_3944/src/ascent-world/SpellEffects.cpp
@@ -3595,7 +3595,7 @@ void Spell::SpellEffectPickpocket(uint32 i) // pickpocket
 		return;
 
 	Creature *target = static_cast<Creature*>( unitTarget );
-	if(target->IsPickPocketed() || (target->GetCreatureName() && lootmgr.IsPickpocketable(target->GetCreatureName()->Id)))
+	if(target->IsPickPocketed() || !lootmgr.IsPickpocketable( target->GetEntry() ) )
 	{
 		SendCastResult(SPELL_FAILED_TARGET_NO_POCKETS);
 		return;
