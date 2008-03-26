4c7371820ec420cd2e5a6e93d2bc9dc3e7c552a3
teh.H4ck0r@gmail.com <teh.H4ck0r@gmail.com@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-03-26 12:18:35 +0000

    - Commiting project files i missed last time

--- trunk/trunk_3944/src/scripts/src/MiscScripts/Setup.cpp
+++ trunk/trunk_3944/src/scripts/src/MiscScripts/Setup.cpp
@@ -3,12 +3,12 @@
 
 extern "C" SCRIPT_DECL uint32 _exp_get_version()
 {
-    return MAKE_SCRIPT_VERSION(SCRIPTLIB_VERSION_MAJOR, SCRIPTLIB_VERSION_MINOR);
+	return MAKE_SCRIPT_VERSION(SCRIPTLIB_VERSION_MAJOR, SCRIPTLIB_VERSION_MINOR);
 }
 
 extern "C" SCRIPT_DECL void _exp_script_register(ScriptMgr* mgr)
 {
-    //SetupDJ(mgr);
+	//SetupDJ(mgr);
 	SetupGameobjects(mgr);
 	//SetupSCRIPT(mgr);
 }
@@ -17,7 +17,7 @@ extern "C" SCRIPT_DECL void _exp_script_register(ScriptMgr* mgr)
 
 BOOL APIENTRY DllMain( HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
 {
-    return TRUE;
+	return TRUE;
 }
 
 #endif
