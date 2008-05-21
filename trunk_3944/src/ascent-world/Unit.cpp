073bbefa19404ffe52ddbed306e0165785b84043
alex_portnov <alex_portnov@yahoo.com>
2008-05-21 07:08:55 +0000

    + T4 priest 2 item set bonus will proc only on prayer of healing from now on. + Added list of all available and updated SpellGroupRelation.

--- trunk_3944/src/ascent-world/Unit.cpp
+++ trunk_3944/src/ascent-world/Unit.cpp
@@ -806,13 +806,19 @@ void Unit::HandleProc( uint32 flag, Unit* victim, SpellEntry* CastingSpell, uint
 								CastingSpell->NameHash != SPELL_HASH_MANGLE__CAT_ )
 								continue;
 						}break;
+						case 37563: //Renewal ( T4 priest healing set 2 items bonus )
+						{
+							if( CastingSpell == NULL )
+								continue;
+							if( CastingSpell->NameHash != SPELL_HASH_PRAYER_OF_HEALING )
+								continue;
+						}break;
 						case 32747: //Deadly Throw Interrupt (rogue arena gloves set)
 						{
 							if( CastingSpell == NULL )
 								continue;
 							if( CastingSpell->NameHash != SPELL_HASH_DEADLY_THROW )
 								continue;
-							printf("gothere!!\n");
 						}break;
 						case 43855: //XXX Libram of Vengeance triggered proc
                         {
