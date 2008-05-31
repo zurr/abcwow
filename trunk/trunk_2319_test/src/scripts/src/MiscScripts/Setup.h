e41556e55930f3eb9e4de63c26439e40a84cbbd8
adamec.michal <adamec.michal@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-05-31 01:48:01 +0000

    - removed 2 old trunks (fresh checkout took ages for nothing)

--- trunk/trunk_2319_test/src/scripts/src/MiscScripts/Setup.h
+++
@@ -1,8 +0,0 @@
-#ifndef INSTANCE_SCRIPTS_SETUP_H
-#define INSTANCE_SCRIPTS_SETUP_H
-
-//void SetupDJ(ScriptMgr * mgr);
-void SetupGameobjects(ScriptMgr * mgr);
-//void SetupSCRIPT(ScriptMgr * mgr);
-
-#endif
