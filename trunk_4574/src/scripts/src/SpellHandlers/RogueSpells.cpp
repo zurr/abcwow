0b65bb1fd3fbf8396cd43a3f796545fae86b7bb8
adamec.michal <adamec.michal@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-07-31 08:24:31 +0000

    * ported back fix for premed resetting cooldown only on correct skills

--- trunk_4574/src/scripts/src/SpellHandlers/RogueSpells.cpp
+++ trunk_4574/src/scripts/src/SpellHandlers/RogueSpells.cpp
@@ -24,9 +24,19 @@ bool Preparation(uint32 i, Spell * pSpell)
     if( playerTarget == NULL ) 
 		return true;
 
-	playerTarget->ClearCooldownsOnLine(39, pSpell->m_spellInfo->Id); // line - subtlety
+	uint32 ClearSpellId[11] = {5277,26669,2983,8696,11305,1856,1857,26889,14177,14183,36554};
+
+	for(i = 0; i < 11; ++i)
+	{
+		if( playerTarget->HasSpell( ClearSpellId[i] ) )
+			playerTarget->ClearCooldownForSpell( ClearSpellId[i] );
+	}
+
+	/*
+    playerTarget->ClearCooldownsOnLine(39, pSpell->m_spellInfo->Id); // line - subtlety
     playerTarget->ClearCooldownsOnLine(38, pSpell->m_spellInfo->Id); // line - combat
     playerTarget->ClearCooldownsOnLine(253, pSpell->m_spellInfo->Id);// line - assassination
+	*/
     return true;
 }
 
