3ebd00ac0bb621345ddfb28b01f303377751eb89
alex_portnov@yahoo.com <alex_portnov@yahoo.com@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-10-08 12:12:36 +0000

    + cleaning up old and unused files.

--- trunk/trunk_3944/src/scripts/src/GossipScripts/Setup.h
+++
@@ -1,9 +0,0 @@
-#ifndef INSTANCE_SCRIPTS_SETUP_H
-#define INSTANCE_SCRIPTS_SETUP_H
-
-void SetupInnkeepers(ScriptMgr * mgr);
-void SetupGuardGossip(ScriptMgr * mgr);
-void SetupBattlemaster(ScriptMgr * mgr);
-void SetupTokenVendor(ScriptMgr * mgr);
-
-#endif
