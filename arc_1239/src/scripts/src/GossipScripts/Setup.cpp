e99f461b43a86ab74115e8e9501e0aeb3703b802
alex_portnov <alex_portnov@yahoo.com>
2008-10-08 13:49:22 +0000

    + Added our scripts. Automake files updated, please do not forget co practice this in the future.

--- arc_1239/src/scripts/src/GossipScripts/Setup.cpp
+++ arc_1239/src/scripts/src/GossipScripts/Setup.cpp
@@ -11,8 +11,12 @@ extern "C" SCRIPT_DECL uint32 _exp_get_script_type()
 extern "C" SCRIPT_DECL void _exp_script_register(ScriptMgr* mgr)
 {
     SetupInnkeepers(mgr);
-    SetupBattlemaster(mgr);
     SetupGuardGossip(mgr);
+    SetupTokenVendor(mgr);
+	SetupTrainerScript(mgr);
+	SetupMulgoreGossip(mgr);
+	SetupShattrathGossip(mgr);
+	SetupTanarisGossip(mgr);
 }
 
 #ifdef WIN32
