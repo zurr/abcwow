f701748251d9fcae609b716dda62fcc9505c4979
alex_portnov@yahoo.com <alex_portnov@yahoo.com@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-10-09 15:25:57 +0000

    + Updating upto 1252

--- trunk/arc_1239/src/arcemu-world/SpellAuras.cpp
+++ trunk/arc_1239/src/arcemu-world/SpellAuras.cpp
@@ -775,7 +775,9 @@ void Aura::Remove()
 		}
 	}
 
-	m_target->m_auras[ m_auraSlot ] = NULL;
+	//maybe we are removing it without even assigning it. Example when we are refreshing an aura
+	if( m_auraSlot != 0xFFFF )
+		m_target->m_auras[ m_auraSlot ] = NULL;
 
 	// reset diminishing return timer if needed
 	::UnapplyDiminishingReturnTimer( m_target, m_spellProto );
@@ -892,7 +894,7 @@ void Aura::UpdateModifiers( )
 
 		if(mod->m_type<TOTAL_SPELL_AURAS)
 		{
-			sLog.outDebug( "WORLD: target = %u , Spell Aura id = %u (%s), SpellId  = %u, i = %u, duration = %u, damage = %d",
+			sLog.outDebug( "WORLD: Update Aura mods : target = %u , Spell Aura id = %u (%s), SpellId  = %u, i = %u, duration = %u, damage = %d",
 				m_target->GetLowGUID(),mod->m_type, SpellAuraNames[mod->m_type], m_spellProto->Id, mod->i, GetDuration(),mod->m_amount);
 			switch (mod->m_type)
 			{
