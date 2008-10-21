a85a95406d1c0009c6e7906eb055a6286a966252
adamec.michal <adamec.michal@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-10-21 16:06:56 +0000

    starting branch for core merge

---
+++ trunk/ABCXaos/src/scripts/src/GossipScripts/Setup.h
@@ -0,0 +1,12 @@
+#ifndef GOSSIP_SCRIPTS_SETUP_H
+#define GOSSIP_SCRIPTS_SETUP_H
+
+void SetupInnkeepers(ScriptMgr * mgr);
+void SetupGuardGossip(ScriptMgr * mgr);
+void SetupTokenVendor(ScriptMgr * mgr);
+void SetupTrainerScript(ScriptMgr * mgr);
+void SetupMulgoreGossip(ScriptMgr * mgr);
+void SetupShattrathGossip(ScriptMgr * mgr);
+void SetupTanarisGossip(ScriptMgr * mgr);
+
+#endif
