8cd6c9cc0a3feedbea126d5ef12b768be608211e
maly32167@wp.pl <maly32167@wp.pl@23d58eba-e73e-0410-97c2-2b01e85c642f>
2008-10-24 23:01:01 +0000

    Added Gaeriyan to IsSpiritHealer ( abit hacky but its how CanSee works ( He is for quest when You have to die and speak while You are ghost )

--- trunk/ABCXaos/src/arcemu-world/Unit.h
+++ trunk/ABCXaos/src/arcemu-world/Unit.h
@@ -1230,6 +1230,7 @@ public:
 		case 6491:  // Spirit Healer
 		case 13116: // Alliance Spirit Guide
 		case 13117: // Horde Spirit Guide
+		case 9299:  // Gaeriyan (Qnpc)
 			{
 				return true;
 			}break;
